92118293c59721be84445beb1ed7c1eba532df04
Магдиев <elnarmag263@gmail.com>
2020-06-17 16:06:40 +0300

    add role, edit users

--- src/main/java/ru/magdiev/springExample/controllers/UserController.java
+++ src/main/java/ru/magdiev/springExample/controllers/UserController.java
@@ -1,16 +1,22 @@
 package ru.magdiev.springExample.controllers;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import ru.magdiev.springExample.entity.Role;
 import ru.magdiev.springExample.entity.User;
 import ru.magdiev.springExample.repositories.UserRepository;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     private final UserRepository userRepository;
 
@@ -23,5 +29,34 @@ public class UserController {
         List<User> users = userRepository.findAll();
         model.addAttribute("users", users);
         return "userList";
+    }
+
+    @GetMapping("{user}")
+    public String userEditForm(@PathVariable User user, Model model) {
+        model.addAttribute("userEdit", user);
+        model.addAttribute("roles", Role.values());
+        return "userEdit";
+    }
+
+    @PostMapping
+    public String userSave(@RequestParam String username,
+            @RequestParam Map<String, String> form,
+            @RequestParam("userId") User user) {
+
+        user.setUsername(username);
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key: form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+        }
+        userRepository.save(user);
+
+        return "redirect:/user";
     }
 }
